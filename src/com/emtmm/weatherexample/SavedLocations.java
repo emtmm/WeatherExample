f9f106480342d62d086fdf3f26ff64c8b0275d6e
emtmm <ernes-tutu@hotmail.com>
2013-04-18 10:28:24 -0400

    Initial commit

---
+++ src/com/emtmm/weatherexample/SavedLocations.java
@@ -0,0 +1,22 @@
+package com.emtmm.weatherexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SavedLocations extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_saved_locations);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_saved_locations, menu);
+		return true;
+	}
+
+}
